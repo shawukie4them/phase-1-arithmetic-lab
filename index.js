f41f74262f77da9216f10ecaa58260266da27a3f
shawukie4them <shawukiekawuki@gmail.com>
2024-07-01 11:31:25 +0300

    operators

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+//Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+//Random
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+//Modulus
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+//Max
+const numbers = [5, 10, 15, 20];
+const max = Math.max(...numbers);
+console.log(max);
